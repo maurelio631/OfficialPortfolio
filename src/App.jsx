56a1b6f2527145951d7507a323f2265bee786065
maurelio631 <marco.silva631@outlook.com>
2022-05-23 21:29:57 -0300

    Criando estrutura, adicionando arquivos de imagem e header com navbar mobile finalizado

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import NavBar from './Components/NavBar';
+import MainContainer from './Components/MainContainer'
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <MainContainer/>
+    </>
+  )
+
+}
+
+export default App
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
+import { AiOutlineMail } from 'react-icons/ai';
+import Teclado from "../Assets/img/teclado.png";
+import Like from "../Assets/img/LikeMemoji.webp"
+
+
+export default function NavBar() {
+    return (
+        <header >
+            <nav className='flex flex-col justify-center items-center bg-black text-white text-base py-5'>
+                <img src="" alt="" />
+                <ul className='flex items-center p-2 flex-col lg:flex-row'>
+                    <li className='p-4'>
+                        <a href="">Sobre mim </a></li>
+                    <li className='p-4'><a href=""> Educação</a></li>
+                    <li className='p-4'><a>Experiência</a></li>
+                    <li className='p-4'><a>Habilidades</a></li>
+                    <li className='p-4'>
+                        <div className='flex'>
+                            <a> <FaLinkedinIn fontSize="1.7rem" className='mx-4' /></a>
+                            <a><FaGithub fontSize="1.7rem" className='mx-4' /></a>
+                            <a><AiOutlineMail fontSize="1.7rem" className='mx-4' /></a>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+            <div className='relative text-center'>
+                <img src={Teclado} alt="teclado" className='h-80'/>
+                <div className='absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 flex'>
+                    <div className='flex flex-col items-center'>
+                        <img src={Like} typeof="image/webp" alt="Memoji sorrindo e fazendo positivo com a mão" className='w-32' />
+                        <h2 className='text-white font mb-3'>Marco Aurélio Sousa Silva</h2>
+                        <p className='text-white'>Desenvolvedor Fullstack</p>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
