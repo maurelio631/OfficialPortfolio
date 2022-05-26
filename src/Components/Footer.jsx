b53869088bf601b7124920a0a293164c0c9e9029
maurelio631 <marco.silva631@outlook.com>
2022-05-25 21:14:37 -0300

    Identação

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,24 +1,36 @@
 import React from "react";
 
-import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
-import { AiOutlineMail } from 'react-icons/ai';
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
 
-import FooterBackground from "../Assets/img/FooterBackground.png"
-import TechMemoji from "../Assets/img/TechMemoji.webp"
+import FooterBackground from "../Assets/img/FooterBackground.png";
+import TechMemoji from "../Assets/img/TechMemoji.webp";
 
 export default function Footer() {
-    return (
-        <footer className="flex justify-center items-center  text-white bg-black">
-            <div className="flex flex-col justify-center items-center my-4">
-                <p className="my-4">Se quiser, entre em contato comigo</p>
-                <div className="flex my-4">
-                    <a href="https://www.linkedin.com/in/marco-aur%C3%A9lio-449459109/" target="_blank"> <FaLinkedinIn href="" className="m-3" fontSize={30} /></a>
-                    <a href="https://github.com/maurelio631" target="_blank"><FaGithub className="m-3" fontSize={30} /></a>
-                    <a href="mailto:marco.silva631@outlook.com"><AiOutlineMail className="m-3" fontSize={30} /></a>
-                </div>
-                <img src={TechMemoji} className="w-32 my-4" />
-                <p className="text-center">Todos os direitos reservados &copy; 2022 - Design created by @linhas.soutas &reg;</p>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className="flex justify-center items-center  text-white bg-black">
+      <div className="flex flex-col justify-center items-center my-4">
+        <p className="my-4">Se quiser, entre em contato comigo</p>
+        <div className="flex my-4">
+          <a
+            href="https://www.linkedin.com/in/marco-aur%C3%A9lio-449459109/"
+            target="_blank"
+          >
+            <FaLinkedinIn href="" className="m-3" fontSize={30} />
+          </a>
+          <a href="https://github.com/maurelio631" target="_blank">
+            <FaGithub className="m-3" fontSize={30} />
+          </a>
+          <a href="mailto:marco.silva631@outlook.com">
+            <AiOutlineMail className="m-3" fontSize={30} />
+          </a>
+        </div>
+        <img src={TechMemoji} className="w-32 my-4" />
+        <p className="text-center">
+          Todos os direitos reservados &copy; 2022 - Design created by
+          @linhas.soutas &reg;
+        </p>
+      </div>
+    </footer>
+  );
 }
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,40 +1,61 @@
-import React from 'react';
-import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
-import { AiOutlineMail } from 'react-icons/ai';
+import React from "react";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
 import Teclado from "../Assets/img/teclado.png";
-import Like from "../Assets/img/LikeMemoji.webp"
-
+import Like from "../Assets/img/LikeMemoji.webp";
 
 export default function NavBar() {
-    return (
-        <header>
-            <nav className='flex flex-col justify-center md:justify-around items-center bg-black text-white text-base py-5'>
-                <img src="" alt="" />
-                <ul className='flex items-center p-2 flex-col lg:flex-row'>
-                    <li className='p-4'>
-                        <a href="">Sobre mim </a></li>
-                    <li className='p-4'><a href=""> Educação</a></li>
-                    <li className='p-4'><a>Experiência</a></li>
-                    <li className='p-4'><a>Habilidades</a></li>
-                    <li className='p-4'>
-                        <div className='flex'>
-                            <a href='https://www.linkedin.com/in/marco-aur%C3%A9lio-449459109/' target="_blank"> <FaLinkedinIn fontSize="1.7rem" className='mx-4' /></a>
-                            <a href='https://github.com/maurelio631' target="_blank"><FaGithub fontSize="1.7rem" className='mx-4' /></a>
-                            <a href='mailto:marco.silva631@outlook.com'><AiOutlineMail fontSize="1.7rem" className='mx-4' /></a>
-                        </div>
-                    </li>
-                </ul>
-            </nav>
-            <div className='relative text-center'>
-                <img src={Teclado} alt="teclado" className='h-80 w-full' />
-                <div className='absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 flex'>
-                    <div className='flex flex-col items-center'>
-                        <img src={Like} typeof="image/webp" alt="Memoji sorrindo e fazendo positivo com a mão" className='w-32' />
-                        <h2 className='text-white font mb-3'>Marco Aurélio Sousa Silva</h2>
-                        <p className='text-white'>Desenvolvedor Fullstack</p>
-                    </div>
-                </div>
+  return (
+    <header>
+      <nav className="flex flex-col justify-center md:justify-around items-center bg-black text-white text-base py-5">
+        <img src="" alt="" />
+        <ul className="flex items-center p-2 flex-col lg:flex-row">
+          <li className="p-4">
+            <a href="">Sobre mim </a>
+          </li>
+          <li className="p-4">
+            <a href=""> Educação</a>
+          </li>
+          <li className="p-4">
+            <a>Experiência</a>
+          </li>
+          <li className="p-4">
+            <a>Habilidades</a>
+          </li>
+          <li className="p-4">
+            <div className="flex">
+              <a
+                href="https://www.linkedin.com/in/marco-aur%C3%A9lio-449459109/"
+                target="_blank"
+              >
+                {" "}
+                <FaLinkedinIn fontSize="1.7rem" className="mx-4" />
+              </a>
+              <a href="https://github.com/maurelio631" target="_blank">
+                <FaGithub fontSize="1.7rem" className="mx-4" />
+              </a>
+              <a href="mailto:marco.silva631@outlook.com">
+                <AiOutlineMail fontSize="1.7rem" className="mx-4" />
+              </a>
             </div>
-        </header>
-    )
+          </li>
+        </ul>
+      </nav>
+      <div className="relative text-center">
+        <img src={Teclado} alt="teclado" className="h-80 w-full" />
+        <div className="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 flex">
+          <div className="flex flex-col items-center">
+            <img
+              src={Like}
+              typeof="image/webp"
+              alt="Memoji sorrindo e fazendo positivo com a mão"
+              className="w-32"
+            />
+            <h2 className="text-white font mb-3">Marco Aurélio Sousa Silva</h2>
+            <p className="text-white">Desenvolvedor Fullstack</p>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
