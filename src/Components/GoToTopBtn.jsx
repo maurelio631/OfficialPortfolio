ded3c2d97c92d53d9efdb9594c676a3d8004d602
maurelio631 <marco.silva631@outlook.com>
2022-05-26 11:19:52 -0300

    Ajustando estilo botão go to top

--- src/Components/GoToTopBtn.jsx
+++ src/Components/GoToTopBtn.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { AiOutlineArrowUp } from "react-icons/ai";
 
 export default function GoToTopBtn() {
   const [backToTop, setBackToTop] = useState(false);
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      if (window.scrollY > 100) {
+      if (window.scrollY > 300) {
         setBackToTop(true);
       } else {
         setBackToTop(false);
@@ -28,7 +29,10 @@ export default function GoToTopBtn() {
           className="fixed bottom-12 right-12 h-12 w-12 text-5xl"
           onClick={scrollUp}
         >
-          ^
+          <AiOutlineArrowUp
+            fontSize={30}
+            className="bg-black rounded-full text-lime-500 hover:bg-lime-500 hover:text-black transition-colors" aria-aria-hidden="true"
+          />
         </button>
       )}
     </div>
