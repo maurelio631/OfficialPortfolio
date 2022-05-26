a9edebaa9cd2c229e4d00ae393b81e0e9cabde6d
maurelio631 <marco.silva631@outlook.com>
2022-05-26 12:29:17 -0300

    Linkando para o portfólio do designer criador do design do site no footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -17,16 +17,29 @@ export default function Footer() {
             href="https://www.linkedin.com/in/marco-aur%C3%A9lio-449459109/"
             target="_blank"
           >
-            <FaLinkedinIn href="" className="m-3" fontSize={30} />
+            <FaLinkedinIn
+              href=""
+              className="m-3 hover:scale-125 transition-all"
+              fontSize={30}
+            />
           </a>
           <a href="https://github.com/maurelio631" target="_blank">
-            <FaGithub className="m-3" fontSize={30} />
+            <FaGithub
+              className="m-3 hover:scale-125 transition-all"
+              fontSize={30}
+            />
           </a>
           <a href="https://twitter.com/ymarc0" target="_blank">
-            <FiTwitter className="m-3" fontSize={30} />
+            <FiTwitter
+              className="m-3 hover:scale-125 transition-all"
+              fontSize={30}
+            />
           </a>
           <a href="mailto:marco.silva631@outlook.com">
-            <AiOutlineMail className="m-3" fontSize={30} />
+            <AiOutlineMail
+              className="m-3 hover:scale-125 transition-all"
+              fontSize={30}
+            />
           </a>
         </div>
         <a href="#">
@@ -34,7 +47,9 @@ export default function Footer() {
         </a>
         <p className="text-center my-5">
           Todos os direitos reservados &copy; 2022 - Design created by
-          @linhas.soutas &reg;
+          <span>
+            <a href="https://linkr.bio/linhas.soutas" target="_blank" className="hover:text-pink-400"> @linhas.soutas&reg; </a>
+          </span>
         </p>
       </div>
     </footer>
